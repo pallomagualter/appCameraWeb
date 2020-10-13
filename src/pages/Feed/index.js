8c9716b751dbb85d99930b6b839c79a88e79c52a
Palloma Gualter <pallomagualter@gmail.com>
2020-10-13 16:33:57 -0300

    Feed

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -11,8 +11,63 @@ import send from '../../assets/send.svg';
 class Feed extends Component {
     render() {
         return (
-           <section>
-           </section>    
+            <section id="post-list">
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Palloma Gualter</span>
+                            <span className="place">Teresina</span>
+                        </div>
+
+                        <img src={more} alt="Mais" />
+                    </header>
+
+                    <img src="http://localhost:3333/files/papa-1602183533610.jpg" alt="Self"/>
+
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt="like"/>
+                            <img src={comment} alt="comment"/>
+                            <img src={send} alt="send"/>
+                        </div>
+
+                        <strong>777 curtidas</strong>
+
+                        <p>
+                            Aquela self!
+                            <span>#Fotinha #Cool #Happy #Top</span>
+                        </p>
+                    </footer>
+                </article>
+
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Palloma Gualter</span>
+                            <span className="place">Teresina</span>
+                        </div>
+
+                        <img src={more} alt="Mais" />
+                    </header>
+
+                    <img src="http://localhost:3333/files/foto1-1602253901027.jpg" alt="Self"/>
+
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt="like"/>
+                            <img src={comment} alt="comment"/>
+                            <img src={send} alt="send"/>
+                        </div>
+
+                        <strong>787 curtidas</strong>
+
+                        <p>
+                            Sombra e água de coco!
+                            <span>#Praia #Sol #Happy #Top</span>
+                        </p>
+                    </footer>
+                </article>
+            </section>
         );
     }
 }
