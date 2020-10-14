557d132e52f63758fa8d8afc9487a7de40a69929
Palloma Gualter <pallomagualter@gmail.com>
2020-10-14 11:25:24 -0300

    new post

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -5,7 +5,36 @@ import './styles.css';
 class New extends Component {
     render() {
         return (
-            <h1>New</h1>
+            <form id="new-post">
+                <input 
+                    type="text"
+                    name="author"
+                    placeholder="Autor do post"
+                />
+
+                <input 
+                    type="text"
+                    name="place"
+                    placeholder="Local do post"
+                />
+                
+                <input 
+                    type="text"
+                    name="description"
+                    placeholder="Descrição do post"
+                />
+
+                
+                <input 
+                    type="text"
+                    name="hashtags"
+                    placeholder="Hashtags do post"
+                />
+
+                <button type="submit">
+                    Enviar
+                </button>
+            </form>
         );
     }
 }
